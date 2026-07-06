64bce1c7bd7223fa149b7ecac6d1dc582ffe66fd
Peter van der Heijden <contact@peterjarian.dev>
2026-07-06 03:36:35 +0200

    add rpc internal error bridge

--- apps/web/src/api/rpc.ts
+++ apps/web/src/api/rpc.ts
@@ -1,8 +1,24 @@
-import { PlakkApi } from "@plakk/shared/PlakkApi";
+import { RpcError } from "@plakk/shared/RpcError";
+import { InternalServerErrorMiddleware, PlakkApi } from "@plakk/shared/PlakkApi";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
 import { HttpRouter } from "effect/unstable/http";
 import { RpcSerialization, RpcServer } from "effect/unstable/rpc";
+
+const InternalServerErrorLive = Layer.succeed(InternalServerErrorMiddleware)(
+  InternalServerErrorMiddleware.of((effect) =>
+    effect.pipe(
+      Effect.catchDefect(() =>
+        Effect.fail(
+          new RpcError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Something went wrong. Please try again.",
+          }),
+        ),
+      ),
+    ),
+  ),
+);
 
 const PlakkApiHandlers = PlakkApi.toLayer(
   PlakkApi.of({
@@ -15,7 +31,7 @@ const RpcRoutes = RpcServer.layerHttp({
   path: "/api/rpc",
   protocol: "http",
   disableFatalDefects: true,
-}).pipe(Layer.provide([PlakkApiHandlers, RpcSerialization.layerNdjson]));
+}).pipe(Layer.provide([PlakkApiHandlers, InternalServerErrorLive, RpcSerialization.layerNdjson]));
 
 export const { handler: handleRpcRequest } = HttpRouter.toWebHandler(RpcRoutes, {
   disableLogger: true,
--- packages/shared/src/api/RpcError.ts
+++ packages/shared/src/api/RpcError.ts
@@ -1,5 +1,10 @@
 import * as Schema from "effect/Schema";
 
+export const RpcErrorCodeSchema = Schema.Literal("INTERNAL_SERVER_ERROR");
+
+export type RpcErrorCode = typeof RpcErrorCodeSchema.Type;
+
 export class RpcError extends Schema.TaggedErrorClass<RpcError>()("RpcError", {
+  code: RpcErrorCodeSchema,
   message: Schema.String,
 }) {}
